55228ea9568090b8dc2629a0592549fcd12f2940
dangdat1111 <dangdat234678@gmail.com>
2025-08-14 14:41:44 +0700

    add : controllFlow

--- Chap2-CommonProgramingConcepts/controllFlow/src/main.rs
+++ Chap2-CommonProgramingConcepts/controllFlow/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 10;
+    if number < 5{
+        println!("condition was true");
+    }else if number !=0 {
+        println!("condition was true, but not the first one");
+    }
+    else {
+        println!("condition was false");
+    }
+
+    let condition = false;
+    let x = if condition { 5 } else { 6 };
+    println!("The value of x is: {}", x);
+
+
+    // Loop example
+//     loop {
+//         println!("datdepzai");
+//     }
+
+    // Loop with break example
+    let mut count = 0;
+    loop {
+        count += 1;
+        if count == 5 {
+            break;
+        }
+        println!("Count is: {}", count);
+    }
+
+    // While loop example
+    let mut num = 5;
+    while num != 0 {
+        println!("{}!", num);
+        num -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
 }
