47066e7095ad7e22d56a45b311114f4eef9f1f8d
dangdat1111 <dangdat234678@gmail.com>
2025-08-14 13:08:39 +0700

    add: function Bodies

--- Chap2-CommonProgramingConcepts/functions/src/main.rs
+++ Chap2-CommonProgramingConcepts/functions/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+fn another_function(){
+    println!("Another function called!");
 }
+// function parameter
+fn parameter_function(x: &str) {
+    println!("Parameter function called with value: {}", x);
+}
+
+
+
+fn main() {
+    println!("function main() called");
+    another_function();
+    parameter_function("Hello, parameter_function!");
+
+
+    // statements and Expression in function Bodies
+    let x = 5;
+    let y =  {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
+
+}
+
+
+
