dabcc813cbfa578f65aec98614f214918b02f092
dangdat1111 <dangdat234678@gmail.com>
2025-10-12 23:59:58 +0700

    commit : clone-tạo 1 bản sao trong heap

--- Chap3-OwnerShip/owner_ship/src/main.rs
+++ Chap3-OwnerShip/owner_ship/src/main.rs
@@ -6,5 +6,11 @@ fn main() {
 
     println!("{s2}, world");
 
+    // sao chép sâu dữ liệu heap của String
+    // tạo 1 bản sao hoàn chỉnh của dữ liệu heap
+    // => tốn bộ nhớ và thời gian hơn
+    let a1 = String::from("hello");
+    let a2 = a1.clone();
+    println!("{a1}, world");
 
 }
